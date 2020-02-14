f6f711389bb0448aed7e6498070eb1d52dc4082e
7vikpeculiar <bugganasathviksanjeev1998@gmail.com>
2020-02-14 09:44:39 +0530

    Push 2

--- Client.java
+++ Client.java
@@ -4,18 +4,22 @@ public class Client {
 	public static void main(String args[]) {
 		try {
 			ServerInterface st = (ServerInterface) Naming.lookup("rmi://" + args[0] + "/AddService");
-			// if (args[1] != null) {
-			st.addGraph("a", 2);
-			st.addEdge("a", 5, 1, 2);
-			st.addEdge("a", 4, 1, 2);
-			st.addEdge("a", 6, 2, 1);
-			System.out.println(st.print("a"));
-			// } else if (args[2] == "2") {
-			// st.addGraph("a", 2);
-			// st.addEdge("a", 5, 1, 2);
-			// st.addEdge("a", 6, 2, 1);
-			// System.out.println(st.print("a"));
-			// }
+			System.out.println("arg[1} is " + args[1]);
+			if (args[1].equals("a")) {
+				System.out.println("here");
+
+				st.addGraph("a", 2);
+				st.addEdge("a", 5, 1, 2);
+				st.addEdge("a", 4, 1, 2);
+				st.addEdge("a", 6, 2, 1);
+				st.addEdge("a", 4, 2, 1);
+				System.out.println(st.print("a"));
+			} else if ((args[1].equals("aa"))) {
+				// st.addGraph("aa", 2);
+				st.addEdge("a", 5, 1, 2);
+				// st.addEdge("aa", 6, 2, 1);
+				System.out.println(st.print("a"));
+			}
 			// System.out.println(st.print(""));
 		} catch (Exception e) {
 			System.out.println(e);
--- Graph.java
+++ Graph.java
@@ -27,19 +27,24 @@ public class Graph {
     String id;
     int edges = 0;
     int vertices;
+    boolean processing;
 
     Graph(int nodes_count) {
         this.vertices = nodes_count;
+        this.processing = false;
     }
 
     List<Edge> EdgeList = new ArrayList<Edge>();
 
     public void addEdge(int src, int dest, int weight) {
+        while (this.processing == true) {
+
+        }
+        this.processing = true;
         System.out.println("Add Edge");
         Edge new_edge = new Edge(src, dest, weight);
         int index = Collections.binarySearch(this.EdgeList, new_edge);
-        System.out.println("Index is :");
-        System.out.println(index);
+        System.out.println("Index is :" + String.valueOf(index));
         if (index < 0) {
             this.EdgeList.add(Math.abs(index) - 1, new_edge);
         } else {
@@ -47,6 +52,7 @@ public class Graph {
         }
         // this.EdgeList.add(new_edge);
         this.edges++;
+        this.processing = false;
     }
 
     public void print() {
@@ -55,6 +61,11 @@ public class Graph {
         for (int i = 0; i < edges; i++) {
             this.EdgeList.get(i).print();
         }
+    }
+
+    public int getMST()
+    {
+        
     }
 
 }
