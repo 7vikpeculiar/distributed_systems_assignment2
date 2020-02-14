34b30d99160fcba084f61a44ba00dda9798c7984
7vikpeculiar <bugganasathviksanjeev1998@gmail.com>
2020-02-15 00:12:08 +0530

    DS-2 6

--- Server.java
+++ Server.java
@@ -4,7 +4,7 @@ import java.rmi.registry.LocateRegistry;
 public class Server {
 	public static void main(String args[]) {
 		try {
-			String server_uri = "rmi://localhost:" + args[0]+"/AddService";
+			String server_uri = "rmi://127.0.0.1:" + args[0]+"/AddService";
 			ServerInterface addService = new Action();
 			LocateRegistry.createRegistry(Integer.valueOf(args[0]));
 			Naming.rebind(server_uri,addService); // addService object is hosted with name AddService
